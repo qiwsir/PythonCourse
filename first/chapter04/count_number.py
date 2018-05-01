265e5c6ff3950ad4bf7e15c9ce2a2f942d210036
qiwsir <qiwsir@promote.cache-dns.local>
2018-05-01 20:58:19 +0800

    count number

---
+++ first/chapter04/count_number.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+'''
+   the times of each number in tuple.
+   filename: count_number.py 
+'''
+t = (3, 6, 1, 0, 2, 4, 6, 0, 8, 10, 7, 4, 0, 2, 0, 10, 6, 2, 4, 1)
+d = dict()
+for n in t:
+    if n in d:
+        d[n] += 1
+    else:
+        d[n] = 1
+print(d)
