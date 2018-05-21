75516739e0f2abd1a950fe1da8e7b9f76cf5f7c3
Chee Wei <qiwsir@promote.cache-dns.local>
2018-05-21 20:41:57 +0800

    new method

---
+++ first/chapter06/converttog.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+'''
+    Convert from jin to gram.
+    filename: convert2g.py
+'''
+
+class ToGram(float):
+    def __new__(cls, jin=0.0):
+        gram = jin / 2 * 1000
+        return float.__new__(cls, gram)
+
+jin = 2.3
+value = ToGram(jin)
+print("{0}jin = {1:.2f}g".format(jin, value))
