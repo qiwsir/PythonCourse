2a1429a6154bc639975646845f20e41e9e8c0758
qiwsir <qiwsir@gmail.com>
2018-06-02 10:55:26 +0800

    add two question

---
+++ first/chapter06/practice/pro18_code.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+'''
+18. 替换字符串中的某些某部分，比如“This is a book”字符串，将其中的“a”替换为“one”。
+'''
+
+import re
+
+#method 1
+def multiple_replace(text, adict):
+    rx = re.compile('|'.join(map(re.escape, adict)))
+    def one_xlat(match):
+        return adict[match.group(0)]
+    return rx.sub(one_xlat, text)
+
+#method 2
+class make_xlat:
+    def __init__(self, *args, **kwargs):
+        self.adict = dict(*args, **kwargs)
+        self.rx = self.make_rx()
+    def make_rx(self):
+        return re.compile('|'.join(map(re.escape, adict)))
+    def one_xlat(self, match):
+        return self.adict[match.group(0)]
+    def __call__(self, text):
+        return self.rx.sub(self.one_xlat, text)
+
+if __name__ == "__main__":
+    text = "It is a good person."
+    adict = {"a good": "ONE", 'person':"human"}
+    rp = make_xlat(adict)
+    r = rp(text)
+    print(r)
+    # r = multiple_replace(text, adict)
+    # print(text)
+    # print(r)
